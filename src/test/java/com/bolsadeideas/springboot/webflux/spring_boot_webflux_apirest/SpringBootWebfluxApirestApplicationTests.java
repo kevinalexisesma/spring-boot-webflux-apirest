d3fd48327035c60757d57f2636b8d6fce9d4d023
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-03 18:46:10 -0500

    Feat: Initial commit with project

---
+++ src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
