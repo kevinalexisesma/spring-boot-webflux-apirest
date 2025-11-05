2fcb580e14b78d319e549e3c7dfe3f9dbbc03d00
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-11-05 16:18:13 +0000

    Feat(ApiRest): Test endpoint list

--- src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,29 @@
 package com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest.models.documents.Producto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void listarTest() {
+		client.get()
+				.uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Producto.class)
+				.hasSize(9);
 	}
 
 }
