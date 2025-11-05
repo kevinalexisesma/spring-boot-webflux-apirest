830d4b997eacc1fc047fa7912a859a793f26bd40
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-11-05 22:22:04 +0000

    Feat(ApiRest): Test endpoint view details

--- src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/models/dao/ProductoDao.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/models/dao/ProductoDao.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest.models.documents.Producto;
 
+import reactor.core.publisher.Mono;
+
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
 
+    public Mono<Producto> findByNombre(String nombre);
 }
--- src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -10,12 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.spring_boot_webflux_apirest.models.service.ProductoService;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
 	@Autowired
 	private WebTestClient client;
+
+	@Autowired
+	private ProductoService productoService;
 
 	@Test
 	void listarTest() {
@@ -37,4 +44,23 @@ class SpringBootWebfluxApirestApplicationTests {
 		// .hasSize(9);
 	}
 
+	@Test
+	void verTest() {
+		Producto producto = productoService.findByNombre("TV Panasonic Pantalla LCD").block();
+		client.get()
+				.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(Producto.class)
+				.consumeWith(response -> {
+					Producto productoResponse = response.getResponseBody();
+					Assertions.assertThat(productoResponse.getId()).isNotEmpty();
+					Assertions.assertThat(productoResponse.getId().length() > 0).isTrue();
+					Assertions.assertThat(productoResponse.getNombre()).isEqualTo("TV Panasonic Pantalla LCD");
+				});
+		// .jsonPath("$.id").isNotEmpty()
+		// .jsonPath("$.nombre").isEqualTo("TV Panasonic Pantalla LCD");
+	}
 }
