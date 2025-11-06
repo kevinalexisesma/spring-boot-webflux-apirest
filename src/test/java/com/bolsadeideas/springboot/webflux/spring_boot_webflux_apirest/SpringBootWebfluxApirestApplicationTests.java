6794771a9b79195f9e63ad558edab49b04edd884
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-05 20:55:34 -0500

    Feat(ApiRest): Running tests in mock mode

--- src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -20,7 +21,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class SpringBootWebfluxApirestApplicationTests {
 
 	@Autowired
